8c257accd0fc2e1af5d25479553b1336b1198a7c
Jithu-Paul <jithu949592@gmail.com>
2026-09-14 22:30:47 +0530

    Add files via upload

--- add.java
+++ add.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
 public class add {
   JFrame frame;
+  JTextField userField;
+  JLabel userLabel;
   public add()
   {
      frame=new JFrame("Add Item");
-      JLabel title=new JLabel("Add New Item");
-       title.setBounds(100,50,200,30);
-       frame.add(title);
-       frame.setSize(500,400);
-        frame.setLayout(null);
-        frame.setVisible(true);
+      JLabel title=new JLabel("Item");
+      title.setBounds(100,50,200,30);
+      userLabel = new JLabel("Add Item");
+      userLabel.setBounds(100, 100, 100, 30);
+
+      userField = new JTextField();
+      userField.setBounds(150, 100, 150, 30);
+      frame.add(userLabel);
+      frame.add(userField);
+      frame.add(title);
+      frame.setSize(500,400);
+      frame.setLayout(null);
+      frame.setVisible(true);
   }  
 }
